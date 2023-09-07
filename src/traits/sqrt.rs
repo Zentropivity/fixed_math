2cb875d8c5e219c556da800f43d9b5c67eaf417b
Zentropivity <zentropivity@protonmail.com>
2023-09-07 23:37:16 +0200

    Used seq-macro to implement Sqrt trait for fixed

--- src/traits/sqrt.rs
+++ src/traits/sqrt.rs
@@ -1,12 +1,12 @@
-use fixed::types::extra::{LeEqU128, LeEqU16, LeEqU32, LeEqU64, LeEqU8};
 use fixed::{
     FixedI128, FixedI16, FixedI32, FixedI64, FixedI8, FixedU128, FixedU16, FixedU32, FixedU64,
     FixedU8,
 };
 
-use typenum::{IsLess, True, U127, U15, U31, U63, U7};
+use seq_macro::seq;
+use typenum::*;
 
-use crate::sqrt::{sqrt_i, sqrt_u};
+use crate::sqrt::{sqrt_i, sqrt_i1, sqrt_u, sqrt_u0, sqrt_u1};
 
 /// Take square root of a number.
 ///
@@ -17,69 +17,112 @@ pub trait Sqrt {
     fn sqrt(self) -> Self;
 }
 
-//TODO seq_macro it
 macro_rules! impl_sqrt_i {
-    ($f:ident, $leq:ident, $f0:ty) => {
-        impl<N> Sqrt for $f<N>
-        where
-            N: $leq + IsLess<$f0, Output = True>,
-        {
+    ($f:ident, $f0:ty) => {
+        impl Sqrt for $f<$f0> {
             #[inline(always)]
             fn sqrt(self) -> Self {
                 sqrt_i(self)
             }
         }
-        //FIXME this should be doable... would work with const generics and seq_macro implementation, but we are on stable Rust
-        // impl<N> Sqrt for $f<N>
-        // where
-        //     N: Unsigned + IsEqual<$f0, Output = True>,
-        // {
-        //     #[inline(always)]
-        //     fn sqrt(self) -> Self {
-        //         sqrt_i1(self)
-        //     }
-        // }
+    };
+}
+macro_rules! impl_sqrt_i1 {
+    ($f:ident, $f0:ty) => {
+        impl Sqrt for $f<$f0> {
+            #[inline(always)]
+            fn sqrt(self) -> Self {
+                sqrt_i1(self)
+            }
+        }
     };
 }
 
 macro_rules! impl_sqrt_u {
-    ($f:ident, $leq:ident, $f0:ty, $f1:ty) => {
-        impl<N> Sqrt for $f<N>
-        where
-            N: $leq + IsLess<$f0, Output = True>,
-        {
+    ($f:ident, $f0:ty) => {
+        impl Sqrt for $f<$f0> {
             #[inline(always)]
             fn sqrt(self) -> Self {
                 sqrt_u(self)
             }
         }
-        //FIXME this should be doable...
-        // impl Sqrt for $f<$f0> {
-        //     #[inline(always)]
-        //     fn sqrt(self) -> Self {
-        //         sqrt_u1(self)
-        //     }
-        // }
-        // impl Sqrt for $f<$f1> {
-        //     #[inline(always)]
-        //     fn sqrt(self) -> Self {
-        //         sqrt_u0(self)
-        //     }
-        // }
+    };
+}
+macro_rules! impl_sqrt_u1 {
+    ($f:ident, $f0:ty) => {
+        impl Sqrt for $f<$f0> {
+            #[inline(always)]
+            fn sqrt(self) -> Self {
+                sqrt_u1(self)
+            }
+        }
+    };
+}
+macro_rules! impl_sqrt_u0 {
+    ($f:ident, $f0:ty) => {
+        impl Sqrt for $f<$f0> {
+            #[inline(always)]
+            fn sqrt(self) -> Self {
+                sqrt_u0(self)
+            }
+        }
     };
 }
 
-impl_sqrt_i!(FixedI8, LeEqU8, U7);
-impl_sqrt_i!(FixedI16, LeEqU16, U15);
-impl_sqrt_i!(FixedI32, LeEqU32, U31);
-impl_sqrt_i!(FixedI64, LeEqU64, U63);
-impl_sqrt_i!(FixedI128, LeEqU128, U127);
+seq!(N in 0..7 {
+    impl_sqrt_i!(FixedI8, U~N);
+});
+impl_sqrt_i1!(FixedI8, U7);
 
-impl_sqrt_u!(FixedU8, LeEqU8, U7, U8);
-impl_sqrt_u!(FixedU16, LeEqU16, U15, U16);
-impl_sqrt_u!(FixedU32, LeEqU32, U31, U32);
-impl_sqrt_u!(FixedU64, LeEqU64, U63, U64);
-impl_sqrt_u!(FixedU128, LeEqU128, U127, U128);
+seq!(N in 0..15 {
+    impl_sqrt_i!(FixedI16, U~N);
+});
+impl_sqrt_i1!(FixedI16, U15);
+
+seq!(N in 0..31 {
+    impl_sqrt_i!(FixedI32, U~N);
+});
+impl_sqrt_i1!(FixedI32, U31);
+
+seq!(N in 0..63 {
+    impl_sqrt_i!(FixedI64, U~N);
+});
+impl_sqrt_i1!(FixedI64, U63);
+
+seq!(N in 0..127 {
+    impl_sqrt_i!(FixedI128, U~N);
+});
+impl_sqrt_i1!(FixedI128, U127);
+
+seq!(N in 0..7 {
+    impl_sqrt_u!(FixedU8, U~N);
+});
+impl_sqrt_u1!(FixedU8, U7);
+impl_sqrt_u0!(FixedU8, U8);
+
+seq!(N in 0..15 {
+    impl_sqrt_u!(FixedU16, U~N);
+});
+impl_sqrt_u1!(FixedU16, U15);
+impl_sqrt_u0!(FixedU16, U16);
+
+seq!(N in 0..31 {
+    impl_sqrt_u!(FixedU32, U~N);
+});
+impl_sqrt_u1!(FixedU32, U31);
+impl_sqrt_u0!(FixedU32, U32);
+
+seq!(N in 0..63 {
+    impl_sqrt_u!(FixedU64, U~N);
+});
+impl_sqrt_u1!(FixedU64, U63);
+impl_sqrt_u0!(FixedU64, U64);
+
+seq!(N in 0..127 {
+    impl_sqrt_u!(FixedU128, U~N);
+});
+impl_sqrt_u1!(FixedU128, U127);
+impl_sqrt_u0!(FixedU128, U128);
 
 impl Sqrt for f32 {
     #[inline(always)]
